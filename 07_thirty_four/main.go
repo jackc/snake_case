bf267d23b906835303df2694d7621f549ee7bfa7
Jack Christensen <jack@jackchristensen.com>
2016-02-20 12:17:56 -0600

    Filter row[2] possibilities
    Rename rows and columns numbers to be 0 based consistently.

--- 07_thirty_four/main.go
+++ 07_thirty_four/main.go
@@ -7,6 +7,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 type row struct {
@@ -18,10 +19,10 @@ func (r row) String() string {
 	return fmt.Sprintf("%d %d %d %d (%b)", r.values[0], r.values[1], r.values[2], r.values[3], r.bits)
 }
 
-func newRow(i, j, k, l byte) row {
+func newRow(x, y, z, w byte) row {
 	var r row
-	r.values = [4]byte{i, j, k, l}
-	r.bits = (1 << i) | (1 << j) | (1 << k) | (1 << l)
+	r.values = [4]byte{x, y, z, w}
+	r.bits = (1 << x) | (1 << y) | (1 << z) | (1 << w)
 	return r
 }
 
@@ -36,8 +37,42 @@ func (s square) String() string {
 
 const allBitsUsed = 131070 // Bit pattern 11111111111111110
 
-func main() {
-	validRows := make([]row, 0)
+type orderedValidRows []row
+
+func (rows orderedValidRows) filterPrefix1(x byte) orderedValidRows {
+	startIdx := sort.Search(len(rows), func(i int) bool {
+		values := rows[i].values
+		return x <= values[0]
+	})
+
+	rows = rows[startIdx:]
+	for i, r := range rows {
+		if r.values[0] != x {
+			return rows[:i]
+		}
+	}
+
+	return rows
+}
+
+func (rows orderedValidRows) filterPrefix2(x, y byte) orderedValidRows {
+	startIdx := sort.Search(len(rows), func(i int) bool {
+		values := rows[i].values
+		return x < values[0] || (x == values[0] && y <= values[1])
+	})
+
+	rows = rows[startIdx:]
+	for i, r := range rows {
+		if r.values[0] != x || r.values[1] != y {
+			return rows[:i]
+		}
+	}
+
+	return rows
+}
+
+func computeValidRows() orderedValidRows {
+	validRows := make(orderedValidRows, 0)
 
 	for i := byte(1); i <= 16; i++ {
 		for j := byte(1); j <= 16; j++ {
@@ -60,8 +95,14 @@ func main() {
 		}
 	}
 
+	return validRows
+}
+
+func main() {
+	validRows := computeValidRows()
+
 	// for _, r := range validRows {
-	// 	fmt.Println(r)
+	//  fmt.Println(r)
 	// }
 	// return
 
@@ -69,74 +110,84 @@ func main() {
 
 	var validSquares []square
 
-	var l1, l2, l3 int64
+	var l0, l1, l2 int64
 
-	for _, r1 := range validRows {
-		l1++
-		usedBits := r1.bits
-		for _, r2 := range validRows {
-			l2++
-			if r2.bits&usedBits != 0 {
+	for _, r0 := range validRows {
+		l0++
+		usedBits := r0.bits
+
+		for _, r1 := range validRows {
+			l1++
+
+			if r1.bits&usedBits != 0 {
 				continue
 			}
 
-			usedBits := usedBits | r2.bits
-			for _, r3 := range validRows {
-				l3++
-				if r3.bits&usedBits != 0 {
-					continue
-				}
+			usedBits := usedBits | r1.bits
 
-				usedBits := usedBits | r3.bits
+			c0ValidRows := validRows.filterPrefix2(r0.values[0], r1.values[0])
+			c0r2Min := c0ValidRows[0].values[2]
+			c0r2Max := c0ValidRows[len(c0ValidRows)-1].values[2]
 
-				// Compute final row, and ensure that all values are between 1 and 16
-				x := 34 - r1.values[0] - r2.values[0] - r3.values[0]
-				if x < 1 || 16 < x {
-					continue
-				}
-				y := 34 - r1.values[1] - r2.values[1] - r3.values[1]
-				if y < 1 || 16 < y {
-					continue
-				}
-				z := 34 - r1.values[2] - r2.values[2] - r3.values[2]
-				if z < 1 || 16 < z {
-					continue
-				}
-				w := 34 - r1.values[3] - r2.values[3] - r3.values[3]
-				if w < 1 || 16 < w {
-					continue
-				}
+			for i := c0r2Min; i <= c0r2Max; i++ {
+				for _, r2 := range validRows.filterPrefix1(i) {
+					l2++
+					if r2.bits&usedBits != 0 {
+						continue
+					}
 
-				r4 := newRow(x, y, z, w)
+					usedBits := usedBits | r2.bits
 
-				if r4.bits|usedBits == allBitsUsed && // check that all bits are used -- this will find collisions in last calculated row with itself as well as any of the other rows
-					// rows 1, 2, and 3 are guaranteed to == 34 because we pulled them from the list
-					// of known good rows
-					r4.values[0]+r4.values[1]+r4.values[2]+r4.values[3] == 34 && // row 4 was computed so must check
-					r1.values[0]+r2.values[0]+r3.values[0]+r4.values[0] == 34 && // column 1
-					r1.values[1]+r2.values[1]+r3.values[1]+r4.values[1] == 34 && // column 2
-					r1.values[2]+r2.values[2]+r3.values[2]+r4.values[2] == 34 && // column 3
-					r1.values[3]+r2.values[3]+r3.values[3]+r4.values[3] == 34 && // column 4
-					r1.values[0]+r2.values[1]+r3.values[2]+r4.values[3] == 34 && // diagonal 1
-					r1.values[3]+r2.values[2]+r3.values[1]+r4.values[0] == 34 { // diagonal 2
-					validSquares = append(validSquares, square{r1, r2, r3, r4})
-					// fmt.Printf("%v\n\n", square{r1, r2, r3, r4})
+					// Compute final row, and ensure that all values are between 1 and 16
+					x := 34 - r0.values[0] - r1.values[0] - r2.values[0]
+					if x < 1 || 16 < x {
+						continue
+					}
+					y := 34 - r0.values[1] - r1.values[1] - r2.values[1]
+					if y < 1 || 16 < y {
+						continue
+					}
+					z := 34 - r0.values[2] - r1.values[2] - r2.values[2]
+					if z < 1 || 16 < z {
+						continue
+					}
+					w := 34 - r0.values[3] - r1.values[3] - r2.values[3]
+					if w < 1 || 16 < w {
+						continue
+					}
+
+					r3 := newRow(x, y, z, w)
+
+					if r3.bits|usedBits == allBitsUsed && // check that all bits are used -- this will find collisions in last calculated row with itself as well as any of the other rows
+						// rows 0, 1, and 2 are guaranteed to == 34 because we pulled them from the list
+						// of known good rows
+						r3.values[0]+r3.values[1]+r3.values[2]+r3.values[3] == 34 && // row 4 was computed so must check
+						r0.values[0]+r1.values[0]+r2.values[0]+r3.values[0] == 34 && // column 1
+						r0.values[1]+r1.values[1]+r2.values[1]+r3.values[1] == 34 && // column 2
+						r0.values[2]+r1.values[2]+r2.values[2]+r3.values[2] == 34 && // column 3
+						r0.values[3]+r1.values[3]+r2.values[3]+r3.values[3] == 34 && // column 4
+						r0.values[0]+r1.values[1]+r2.values[2]+r3.values[3] == 34 && // diagonal 1
+						r0.values[3]+r1.values[2]+r2.values[1]+r3.values[0] == 34 { // diagonal 2
+						validSquares = append(validSquares, square{r0, r1, r2, r3})
+						// fmt.Printf("%v\n\n", square{r0, r1, r2, r3})
+					}
 				}
 			}
+
 		}
 	}
 
+	fmt.Println("l0:", l0)
 	fmt.Println("l1:", l1)
 	fmt.Println("l2:", l2)
-	fmt.Println("l3:", l3)
 	fmt.Println("Magic squares:", len(validSquares))
 }
 
-// l1: 2064
-// l2: 4260096
-// l3: 2556057600
+// l0: 2064
+// l1: 4260096
+// l2: 1684010304
 // Magic squares: 7040
 
-// real	0m11.939s
-// user	0m12.009s
-// sys	0m0.021s
+// real	0m10.817s
+// user	0m10.868s
+// sys	0m0.043s
