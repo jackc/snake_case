ae82844540ba79e10f220006e82e7bbd3d1b3c97
Jack Christensen <jack@jackchristensen.com>
2016-04-23 10:12:53 -0500

    Benchmarks should validate results

--- 12_8_queens/solver/solver_test.go
+++ 12_8_queens/solver/solver_test.go
@@ -9,7 +9,14 @@ import (
 func BenchmarkSolve8(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		solver := solver.New(8, 8, 8)
+
+		solCount := 0
 		for range solver.SolChan() {
+			solCount++
+		}
+
+		if solCount != 92 {
+			b.Fatalf("Expected 92 solutions, got: %d", solCount)
 		}
 	}
 }
@@ -17,7 +24,14 @@ func BenchmarkSolve8(b *testing.B) {
 func BenchmarkSolve10(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		solver := solver.New(10, 10, 10)
+
+		solCount := 0
 		for range solver.SolChan() {
+			solCount++
+		}
+
+		if solCount != 724 {
+			b.Fatalf("Expected 724 solutions, got: %d", solCount)
 		}
 	}
 }
