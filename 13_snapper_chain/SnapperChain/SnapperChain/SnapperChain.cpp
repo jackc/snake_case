425d88a404a72dda3283ffda342884cda8983ca5
Jack Christensen <jack@jackchristensen.com>
2016-05-10 19:32:18 -0500

    Streamline snapper chain

--- 13_snapper_chain/SnapperChain/SnapperChain/SnapperChain.cpp
+++ 13_snapper_chain/SnapperChain/SnapperChain/SnapperChain.cpp
@@ -5,23 +5,35 @@
 
 #include <iostream>
 
-#include "snapper.h"
-
 using namespace std;
 
+auto solve(int deviceCount, int snapCount)->bool;
 
 int main()
 {
-	auto problems = parseProblems(cin);
+	int problemCount;
+	cin >> problemCount;
 
-	int caseNum = 0;
-	for (auto p : *problems) {
-		caseNum++;
-		cout << "Case #" << caseNum << ": " << (solve(p) ? "ON" : "OFF") << endl;
+	for (int i = 0; i < problemCount; i++) {
+		int deviceCount;
+		int snapCount;
+		
+		cin >> deviceCount >> snapCount;
+		cout << "Case #" << (i+1) << ": " << (solve(deviceCount, snapCount) ? "ON" : "OFF") << endl;
 	}
+
     return 0;
 }
 
+auto solve(int deviceCount, int snapCount)->bool
+{
+	if (snapCount == 0) {
+		return false;
+	}
+
+	unsigned int cycle = 1 << deviceCount;
+	return (snapCount + 1) % cycle == 0;
+}
 
 
 
